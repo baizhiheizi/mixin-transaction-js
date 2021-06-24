5e5e24754f4bb80d6c684894385a306912caab55
an-lee <an.lee.work@gmail.com>
2021-06-24 09:22:49 +0800

    init

---
+++ index.js
@@ -0,0 +1,9 @@
+import * as mixnGo from "./transaction";
+
+const decodeTransaction = mixnGo.decodeTransaction;
+const buildTransaction = mixnGo.buildTransaction;
+
+export default {
+  decodeTransaction,
+  buildTransaction,
+};
